1c9430ab9b79f5c560f91813862292621fcb5706
MKNG18 <naili.maeva@orange.fr>
2025-04-03 16:31:44 +0200

    added props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,12 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+interface PokemonType {
+  pokemon: {
+  imgSrc?: string;
+  name: string;
+};
+}
+function PokemonCard ({pokemon}) {
 
 
-function PokemonCard () {
-    const pokemon = pokemonList[1];
 
 return (
 <figure>
