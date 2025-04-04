e81053e7881568f59b0af86bb1d095c2d94538a4
MKNG18 <naili.maeva@orange.fr>
2025-04-04 16:53:48 +0200

    added button and pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,54 @@ import './App.css'
 import PokemonCard from './components/PokemonCard';
 import {useState} from "react";
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 
 function App() {
-    const [pokemonName, setPokemonName] = useState("bulbasaur");
+  const [pokemonName, setPokemonName] = useState(null);
+   
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   
-    const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
-  
-    if (pokemon == null) {
-      throw new Error("Invalid pokemon name");
- }
+  const selectedPokemon = pokemonList.find((poke) => poke.name === pokemonName);
+
   return (
 
-    <div><PokemonCard pokemon={pokemon}/>
-    <button type="button" onClick={() => setPokemonName ("bulbasaur")}>
-     Bulbasaur
-    </button>
-    <button type="button" onClick={() => setPokemonName ("mew")}>
-       Mew
-    </button>
-    
+    <div>
+
+  <nav>
+    <h1>Pokemon list</h1>
+
+      {pokemonList.map((pokemon) => (<button key={pokemon.name} onClick={() => setPokemonName(pokemon.name)}>{pokemon.name}</button>
+      ))}
+  
+    </nav>
+    {selectedPokemon && <PokemonCard pokemon={selectedPokemon}/>}
     </div>
 
-  );
-}
+    );
+  }
 
 export default App;
